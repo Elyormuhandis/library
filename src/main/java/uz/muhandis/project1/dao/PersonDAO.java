2d577350333481ef982c2cacfadcbd032d75573f
elyormuhandis <elyormuhandis@gmail.com>
2023-05-23 13:05:01 +0500

    initial commit

---
+++ src/main/java/uz/muhandis/project1/dao/PersonDAO.java
@@ -0,0 +1,59 @@
+package uz.muhandis.project1.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import uz.muhandis.project1.models.Book;
+import uz.muhandis.project1.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+    public Optional<Person> show(String fullName) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE full_name=?", new Object[]{fullName}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(full_name, birth_year) VALUES(?, ?)", person.getFullName(), person.getBirthYear());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET full_name=?, birth_year=? WHERE id=?", updatedPerson.getFullName(),
+                updatedPerson.getBirthYear(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    public List<Book> personBooks(int personId) {
+        List<Book> books = jdbcTemplate.query("SELECT * FROM book WHERE person_id=?", new Object[]{personId}, new BeanPropertyRowMapper<>(Book.class));
+        return books;
+    }
+
+
+
+}
---
+++ src/main/java/uz/muhandis/project1/models/Person.java
@@ -0,0 +1,58 @@
+package uz.muhandis.project1.models;
+
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "Full name not should be empty")
+    private String fullName;
+    @NotNull(message = "Year of birth not should be empty")
+    @Min(value = 1900, message = "Year of birth should be greater than 1900")
+    private int birthYear;
+
+    public Person() {
+    }
+
+    public Person(int id, String fullName, int birthYear) {
+        this.id=id;
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", yearOfBirth=" + birthYear +
+                '}';
+    }
+}
+
